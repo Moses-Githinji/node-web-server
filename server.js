964a0a530b19dfdea712bc9998e782758e77d108
Moses Githinji <ndirangugithinji23@gmail.com>
2022-10-27 17:10:58 +0300

    node-web-server first-commit

---
+++ server.js
@@ -0,0 +1,110 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const fsPromises = require('fs').promises;
+const logEvents = require('./logEvents');
+const EventEmitter = require('events');
+
+class Emitter extends EventEmitter {}
+// initialize the object we're going to create
+const myEmmiter = new Emitter();
+
+myEmmiter.on('log', (msg, fileName) => {
+  logEvents(msg, fileName);
+});
+
+// Define the port that our server shall listen to
+const PORT = process.env.PORT || 3500;
+
+const serveFile = async (filePath, contentType, response) => {
+  try {
+    const rawData = await fsPromises.readFile(
+      filePath,
+      !contentType.includes('image') ? 'utf8' : ''
+    );
+    const data =
+      contentType === 'application/json' ? JSON.parse(rawData) : rawData;
+    response.writeHead(filePath.includes('404.html') ? 404 : 200, {
+      'Content-Type': contentType,
+    });
+    response.end(
+      contentType === 'application/json' ? JSON.stringify(data) : data
+    );
+  } catch (error) {
+    console.log(error);
+    myEmmiter.emit('log', `${error.name}: ${error.message}`, 'errorLog.txt');
+    response.statusCode(500); // server error if we couldn't read the data that we want from the server
+    response.end();
+  }
+};
+
+// Create our server
+const server = http.createServer((req, res) => {
+  console.log(`Your request URL is ${req.url}`);
+  console.log(`Your request method is ${req.method}`);
+
+  myEmmiter.emit('log', `${req.url}\t${req.method}`, 'reqLog.txt');
+
+  const extension = path.extname(req.url);
+
+  let contentType;
+
+  switch (extension) {
+    case '.css':
+      contentType = 'text/css';
+      break;
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    case '.json':
+      contentType = 'application/json';
+      break;
+    case '.jpg':
+      contentType = 'image/jpeg';
+      break;
+    case '.png':
+      contentType = 'image/png';
+      break;
+    case '.txt':
+      contentType = 'text/plain';
+      break;
+    default:
+      contentType = 'text/html';
+  }
+
+  let filePath =
+    contentType === 'text/html' && req.url === '/'
+      ? path.join(__dirname, 'views', 'index.html')
+      : contentType === 'text/html' && req.url.slice(-1) === '/'
+      ? path.join(__dirname, 'views', req.url, 'index.html')
+      : contentType === 'text/html'
+      ? path.join(__dirname, 'views', req.url)
+      : path.join(__dirname, req.url);
+
+  // This code makes the .html extension not required in the browser
+  if (!extension && req.url.slice(-1) !== '/') filePath += '.html';
+
+  const fileExists = fs.existsSync(filePath);
+
+  if (fileExists) {
+    // serve the file
+    serveFile(filePath, contentType, res);
+  } else {
+    switch (path.parse(filePath).base) {
+      case 'cars.html':
+        res.writeHead(301, { Location: '/new-page.html' });
+        res.end();
+        break;
+      case 'www-page.html':
+        res.writeHead(301, { Location: '/' });
+        res.end();
+        break;
+      default:
+        // serve a 404 response
+        serveFile(path.join(__dirname, 'views', '404.html'), 'text/html', res);
+    }
+  }
+});
+server.listen(PORT, () =>
+  console.log(`Your server is running on port ${PORT}`)
+);
